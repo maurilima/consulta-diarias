86c466ea63b21fdbfad5b7b8e6ba7fc58dd7dea3
Mauri Lima <mauri.lima.roraima@gmail.com>
2021-08-17 23:58:58 -0400

    Remover Config

--- src/config/Config.js
+++
@@ -1,6 +0,0 @@
-export const BASE_AUTH = 'oauth/token';
-let userPw = 'ewfWEF55465#@#';
-var userName = 'portal_transparencia';
-export let encoded = btoa(userName+':'+userPw);
-export let AUTH = 'Basic ' + encoded;
-
