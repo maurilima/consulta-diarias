314b402e59a74005cde377c3f0cece6ac14281e7
Mauri Lima <mauri.lima.roraima@gmail.com>
2021-08-18 00:02:06 -0400

    TEstando gitIgnore

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,19 +5,15 @@ import { Col, Container, Row } from "react-bootstrap";
 
 function Header() {
     return (
-<Container>
-    <h1>Header</h1>
-  <Row>
-    <Col>1 of 3</Col>
-    <Col xs={6}>2 of 3 (wider)</Col>
-    <Col>3 of 3</Col>
-  </Row>
-
-</Container>        
-       
+        <Container>
+            <h1>Header</h1>
+            <Row>
+                <Col>1 of 3</Col>
+                <Col xs={6}>2 of 3 (wider)</Col>
+                <Col>3 of 3</Col>
+            </Row>
+        </Container>
     )
 }
-
-
 
 export default Header;
